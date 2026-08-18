270e73a595a10ff8b618be804d94eddbceee96c7
SadiaShara <sadiatasnim.stas@gmail.com>
2026-08-18 14:19:30 +0600

    Update DORITO_DATAPATH.v

--- rtl/DORITO_DATAPATH.v
+++ rtl/DORITO_DATAPATH.v
@@ -1,212 +1,189 @@
- 19 //-----------------------------------------------------------------------------
- 20 timeunit 1ns;
- 21 timeprecision 1ps;
- 22
- 23 import DORITO_PKG::*;
- 24
- 25  module DORITO_DATAPATH
- 26 (input logic SYS_CLOCK,
- 27 input  logic [BUS_SIZE-1 : 0] MEM_DATA_OUT,
- 28 input  logic SCLR_PC, INC_PC, LOAD_PC,
- 29 input  logic LOAD_IR,
- 30 input  logic [REG_ADDR_SIZE-1 : 0] SEL_SOURCE_REG1, SEL_SOURCE_REG2, SEL_DEST_REG,
- 31 input  logic REG_FILE_ARESET,LOAD_DEST_REG,
- 32 input  logic LOAD_ACCUM_REG,
- 33 input  logic IMM_OFFSET_SELECTOR,
- 34 input  logic [MNEMONIC_SIZE-1 : 0] ALU_CONTROL,
- 35 input  logic SEL_SOURCE1, SEL_SOURCE2, SEL_DATA, SEL_ADDR,
- 36 input  logic LOAD_ADDR_REG,
- 37 input  logic EN,        //ADDED
- 38
- 39 output  logic [BUS_SIZE-1 : 0] IR_OUT,
- 40 output  logic [BUS_SIZE-1 : 0] ACCUM_REG,
- 41 output  logic CARRYOUT, U_OVF, S_OVF, ALU_OUT_ZERO, ALU_OUT_POS, ALU_OUT_NEG,
- 42 output  logic signed [ADDR_SIZE-1 : 0] MEM_ADDRESS, PC_OUT,
- 43 //MAKE ADDRESS 5 BIT (CAN POINT TO 32 LOCATIONS) FOR EASIER VERIFICATION.
- 44 output  logic [BUS_SIZE-1 : 0] /*MEM_ADDRESS,*/ MEM_DATA_IN
- 45 //output  wire [BUS_SIZE-1 : 0] /*MEM_ADDRESS,*/ MEM_DATA_IN //CHANGE
- 46 );
- 47
- 48 //module DORITO_DATAPATH (.*);           
-   50 //timeunit 1ns;
- 51 //timeprecision 1ps;
- 52
- 53  logic [BUS_SIZE-1 : 0] INT_DATA_BUS, SOURCE_REG1, SOURCE_REG2, IMM_OFFSET_DATA,
- 54 /*PC_OUT, ADDR_REG_OUT,*/ ALU_IN1, ALU_IN2, ALU_OUT;
- 55 //var logic signed [ADDR_SIZE-1 : 0] PC_OUT, ADDR_REG_OUT;
- 56  logic signed [ADDR_SIZE-1 : 0] ADDR_REG_OUT; //CHANGE
- 57 //MAKE PC_OUT & ADDR_REG_OUT 5 BIT (CAN POINT TO 32 LOCATIONS) FOR EASIER VERIFICATION.
- 58 //var logic signed [BUS_SIZE-1 : 0] PC_OUT, ADDR_REG_OUT;
- 59  logic [BUS_SIZE : 0] ALU_OUT_W_CARRY;
- 60  logic [BUS_SIZE-1 : 0] R_FILE [REGISTER_NO-1 : 0];
- 61
- 62 always_ff @ (posedge SYS_CLOCK)
- 63 begin : PC
- 64 if (LOAD_PC)
- 65 PC_OUT <= $signed(INT_DATA_BUS[4:0]);/*INT_DATA_BUS[4:0];*/
- 66 //MAKE PC_OUT 5 BIT (CAN POINT TO 32 LOCATIONS) FOR EASIER VERIFICATION.
- 67 else if (SCLR_PC)
- 68 PC_OUT <= '0;
- 69 else if (INC_PC)
- 70 PC_OUT <= PC_OUT + 1'b1;
- 71 end : PC
- 72
- 73 always_ff @ (posedge SYS_CLOCK)
- 74 begin : IR
- 75 if (LOAD_IR)
- 76 IR_OUT <= INT_DATA_BUS;
- 77 end : IR
-  80 always_ff @ (posedge SYS_CLOCK)
- 81 begin : REG_FILE
- 82 if (REG_FILE_ARESET)
- 83 begin
- 84 R_FILE <= '{default : '0};
- 85 end
- 86 else if (LOAD_DEST_REG)
- 87 R_FILE [SEL_DEST_REG] <= INT_DATA_BUS;
- 88 end : REG_FILE
- 89
- 90 assign SOURCE_REG1 = R_FILE [SEL_SOURCE_REG1];
- 91 assign SOURCE_REG2 = R_FILE [SEL_SOURCE_REG2];
- 92
- 93 always_ff @ (posedge SYS_CLOCK)
- 94 begin : ACC_REG
- 95         if (LOAD_ACCUM_REG)
- 96                 ACCUM_REG <= ALU_OUT;
- 97 end : ACC_REG
- 98
- 99 always_comb
-100 begin : IMMEDIATE_DATA_OFFSET_CONTROLLER
-101 if (IMM_OFFSET_SELECTOR)
-102 IMM_OFFSET_DATA = {'0,IR_OUT[11:0]};
-103 else
-104 //IMM_OFFSET_DATA = {{20{IR_OUT[11]}},IR_OUT[11:0]};
-105 //IMM_OFFSET_DATA = {{27{IR_OUT[14]}},IR_OUT[4:0]};
-106 IMM_OFFSET_DATA = {{27{IR_OUT[4]}},IR_OUT[4:0]};//CHANGE
-107 //TAKE LSB 5 BIT OF IMMEDIATE DATA (AS ADDRESS BUSSES HAS BEEN MADE 5BITS)
-108 //RATHER THAN FULL 12 BITS AND THEN AND SIGN-EXTEND THEM TO 32 BITS
-110 end : IMMEDIATE_DATA_OFFSET_CONTROLLER
-111
-112 always_ff @ (posedge SYS_CLOCK)
-113 begin : ADDRESS_REG
-114 if (REG_FILE_ARESET)
-115 ADDR_REG_OUT <= '0;
-116 else
-117 if (LOAD_ADDR_REG)
-118 ADDR_REG_OUT <= ALU_OUT[ADDR_SIZE-1:0]; /*ALU_OUT;*/
-119 end : ADDRESS_REG
-120
-121 always_comb
-122 begin : ALU
-123 CARRYOUT = '0; U_OVF = '0; S_OVF = '0;
-124 ALU_OUT_W_CARRY = '0;
-125 case (ALU_CONTROL)
-126
-127 ADD                     : begin
-128 ALU_OUT_W_CARRY = $signed(ALU_IN1) + $signed(ALU_IN2);
-129 ALU_OUT = ALU_OUT_W_CARRY[BUS_SIZE-1 : 0];
-130 CARRYOUT = ALU_OUT_W_CARRY[BUS_SIZE];
-131 U_OVF = ALU_OUT_W_CARRY[BUS_SIZE];
-132 S_OVF = ( ~(ALU_IN1[BUS_SIZE-1] ^ ALU_IN2[BUS_SIZE-1])
-133 && (ALU_OUT[BUS_SIZE-1] ^ ALU_IN1[BUS_SIZE-1]) );
-134 end
-135 SUB                     : begin
-136 ALU_OUT_W_CARRY = ALU_IN1 - ALU_IN2;
-137 ALU_OUT = ALU_OUT_W_CARRY[BUS_SIZE-1 : 0];
-138 CARRYOUT = ALU_OUT_W_CARRY[BUS_SIZE];
-  139 U_OVF = ~ALU_OUT_W_CARRY[BUS_SIZE];
-140 S_OVF = ( (ALU_IN1[BUS_SIZE-1] ^ ALU_IN2[BUS_SIZE-1])
-141 && ~(ALU_OUT[BUS_SIZE-1] ^ ALU_IN2[BUS_SIZE-1]) );
-142 end
-143 INC_IN1                 : begin
-144 ALU_OUT_W_CARRY = ALU_IN1 + 1'b1;
-145 ALU_OUT = ALU_OUT_W_CARRY[BUS_SIZE-1 : 0];
-146 CARRYOUT = ALU_OUT_W_CARRY[BUS_SIZE];
-147 U_OVF = ALU_OUT_W_CARRY[BUS_SIZE];
-148 end
-149
-150 INC_IN2                 : begin
-151 ALU_OUT_W_CARRY = ALU_IN2 + 1'b1;
-152 ALU_OUT = ALU_OUT_W_CARRY[BUS_SIZE-1 : 0];
-153 CARRYOUT = ALU_OUT_W_CARRY[BUS_SIZE];
-154 U_OVF = ALU_OUT_W_CARRY[BUS_SIZE];
-155
-156 end
-157 PASS_0          : ALU_OUT = '0;
-158 PASS_IN2        : ALU_OUT = ALU_IN2;
-159 PASS_LOW16_IN2 : ALU_OUT = ALU_IN2[(BUS_SIZE/2)-1 :0];
-160 PASS_HIGH16_IN2 : ALU_OUT = ALU_IN2[BUS_SIZE-1 : (BUS_SIZE/2)];
-161 AND : ALU_OUT = ALU_IN1 & ALU_IN2;
-162 OR  : ALU_OUT = ALU_IN1 | ALU_IN2;
-163 XOR : ALU_OUT = ALU_IN1 ^ ALU_IN2;
-164 NAND  : ALU_OUT = ~(ALU_IN1 & ALU_IN2);
-165 NOR  : ALU_OUT = ~(ALU_IN1 | ALU_IN2);
-166 XNOR : ALU_OUT = ~(ALU_IN1 ^ ALU_IN2);
-167 NOT_IN2 : ALU_OUT = ~ ALU_IN2;
-168 L_SHIFT_IN2 : ALU_OUT = ALU_IN2 << 1;    
-  169 R_SHIFT_IN2 : ALU_OUT = ALU_IN2 >> 1;
-170 S_L_SHIFT_IN2 : ALU_OUT = ALU_IN2 <<< 1;
-171 S_R_SHIFT_IN2  : ALU_OUT = ALU_IN2 >>> 1;
-172 L_ROTATE_IN2 : ALU_OUT = {ALU_IN2[BUS_SIZE-2 : 0], ALU_IN2[BUS_SIZE-1]};
-173 R_ROTATE_IN2    : ALU_OUT = {ALU_IN2[0],ALU_IN2[BUS_SIZE-1 : 1]};
-174
-175 default : ALU_OUT = 'x;
-176
-177 endcase
-178
-179 end : ALU
-180
-181 assign MEM_DATA_IN = ALU_OUT;
-182 //assign MEM_DATA_IN = EN ? ALU_OUT : 'z;
-183
-184 assign ALU_OUT_ZERO = ALU_OUT == '0 ? '1 : '0;
-185 //IF ALU_OUT IS '0, ALU_OUT_ZERO = '1;
-186 assign ALU_OUT_POS = !ALU_OUT[BUS_SIZE-1] ? '1 : '0;
-187 //IF MSB OF ALU_OUT IS '0, ALU_OUT_POS = '1;
-188 assign ALU_OUT_NEG = ALU_OUT[BUS_SIZE-1] ? '1 : '0;
-189 //IF MSB OF ALU_OUT IS '1, ALU_OUT_NEG = '1;
-190
-191 always_comb
-192 begin : MUX_DATA
-193
-194 case (SEL_DATA)
-195 1'b0 : INT_DATA_BUS = MEM_DATA_OUT;
-196 1'b1 : INT_DATA_BUS = ALU_OUT;
-197 default : INT_DATA_BUS = 'x;
-198 endcase 
-200 end     : MUX_DATA
-201
-202 always_comb
-203 begin : MUX_SOURCE1
-204 case (SEL_SOURCE1)
-205 1'b0 : ALU_IN1 = {{27{PC_OUT[4]}},PC_OUT};/*PC_OUT;*/ // PC_OUT HAS BEEN MADE 5 BITS (CAN POINT TO 32 LOCATIONS) FOR EAS    IER VERIFICATION.
-206 1'b1 : ALU_IN1 = SOURCE_REG1;
-207 default : ALU_IN1 = 'x;
-208 endcase
-209 end     : MUX_SOURCE1
-210
-211 always_comb
-212 begin : MUX_SOURCE2
-213
-214 case (SEL_SOURCE2)
-215 1'b0 : ALU_IN2 = SOURCE_REG2;
-216 1'b1 : ALU_IN2 = IMM_OFFSET_DATA;
-217 default : ALU_IN2 = 'x;
-218 endcase
-219
-220 end     : MUX_SOURCE2
-221
-222 always_comb
-223 begin : MUX_ADDR
-224
-225 case (SEL_ADDR)
-226 1'b0 : MEM_ADDRESS = PC_OUT;
-227 1'b1 : MEM_ADDRESS = ADDR_REG_OUT;
-228 default : MEM_ADDRESS = 'x;   
-229 endcase
-230
-231 end     : MUX_ADDR
-232
-233 //assign MEM_ADDRESS = ADDR_REG_OUT;
-234
+  timeunit 1ns;
+  timeprecision 1ps;
+  import DORITO_PKG::*;
+
+  module DORITO_DATAPATH
+  (input logic SYS_CLOCK,
+  input  logic [BUS_SIZE-1 : 0] MEM_DATA_OUT,
+  input  logic SCLR_PC, INC_PC, LOAD_PC,
+  input  logic LOAD_IR,
+  input  logic [REG_ADDR_SIZE-1 : 0] SEL_SOURCE_REG1, SEL_SOURCE_REG2, SEL_DEST_REG,
+  input  logic REG_FILE_ARESET,LOAD_DEST_REG,
+  input  logic LOAD_ACCUM_REG,
+  input  logic IMM_OFFSET_SELECTOR,
+  input  logic [MNEMONIC_SIZE-1 : 0] ALU_CONTROL,
+  input  logic SEL_SOURCE1, SEL_SOURCE2, SEL_DATA, SEL_ADDR,
+  input  logic LOAD_ADDR_REG,
+  input  logic EN,        //ADDED
+   
+  output  logic [BUS_SIZE-1 : 0] IR_OUT,
+  output  logic [BUS_SIZE-1 : 0] ACCUM_REG,
+  output  logic CARRYOUT, U_OVF, S_OVF, ALU_OUT_ZERO, ALU_OUT_POS, ALU_OUT_NEG,
+  output  logic signed [ADDR_SIZE-1 : 0] MEM_ADDRESS, PC_OUT,
+  //MAKE ADDRESS 5 BIT (CAN POINT TO 32 LOCATIONS) FOR EASIER VERIFICATION.
+  output  logic [BUS_SIZE-1 : 0] /*MEM_ADDRESS,*/ MEM_DATA_IN
+  //output  wire [BUS_SIZE-1 : 0] /*MEM_ADDRESS,*/ MEM_DATA_IN //CHANGE
+  );
+ 
+  
+  logic [BUS_SIZE-1 : 0] INT_DATA_BUS, SOURCE_REG1, SOURCE_REG2, IMM_OFFSET_DATA,
+  logic signed [ADDR_SIZE-1 : 0] ADDR_REG_OUT; //CHANGE
+ 
+  logic [BUS_SIZE-1 : 0] R_FILE [REGISTER_NO-1 : 0];
+  always_ff @ (posedge SYS_CLOCK)
+  begin : PC
+  if (LOAD_PC)
+  PC_OUT <= $signed(INT_DATA_BUS[4:0]);/*INT_DATA_BUS[4:0];*/
+  //MAKE PC_OUT 5 BIT (CAN POINT TO 32 LOCATIONS) FOR EASIER VERIFICATION.
+  else if (SCLR_PC)
+  PC_OUT <= '0;
+  else if (INC_PC)
+  PC_OUT <= PC_OUT + 1'b1;
+  end : PC
+ 
+  always_ff @ (posedge SYS_CLOCK)
+  begin : IR
+  if (LOAD_IR)
+  IR_OUT <= INT_DATA_BUS;
+  end : IR
+  always_ff @ (posedge SYS_CLOCK)
+  begin : REG_FILE
+  if (REG_FILE_ARESET)
+  begin
+  R_FILE <= '{default : '0};
+  end
+  else if (LOAD_DEST_REG)
+  R_FILE [SEL_DEST_REG] <= INT_DATA_BUS;
+  end : REG_FILE
+ 
+ assign SOURCE_REG1 = R_FILE [SEL_SOURCE_REG1];
+ assign SOURCE_REG2 = R_FILE [SEL_SOURCE_REG2];
+
+ always_ff @ (posedge SYS_CLOCK)
+ begin : ACC_REG
+  if (LOAD_ACCUM_REG)
+         ACCUM_REG <= ALU_OUT;
+ end : ACC_REG
+ 
+ always_comb
+  begin : IMMEDIATE_DATA_OFFSET_CONTROLLER
+ if (IMM_OFFSET_SELECTOR)
+ IMM_OFFSET_DATA = {'0,IR_OUT[11:0]};
+ else
+  
+end : IMMEDIATE_DATA_OFFSET_CONTROLLER
+
+always_ff @ (posedge SYS_CLOCK)
+begin : ADDRESS_REG
+if (REG_FILE_ARESET)
+ADDR_REG_OUT <= '0;
+else
+if (LOAD_ADDR_REG)
+ADDR_REG_OUT <= ALU_OUT[ADDR_SIZE-1:0]; /*ALU_OUT;*/
+end : ADDRESS_REG
+
+always_comb
+begin : ALU
+CARRYOUT = '0; U_OVF = '0; S_OVF = '0;
+ALU_OUT_W_CARRY = '0;
+case (ALU_CONTROL)
+
+ADD: begin
+ALU_OUT_W_CARRY = $signed(ALU_IN1) + $signed(ALU_IN2);
+ALU_OUT = ALU_OUT_W_CARRY[BUS_SIZE-1 : 0];
+CARRYOUT = ALU_OUT_W_CARRY[BUS_SIZE];
+U_OVF = ALU_OUT_W_CARRY[BUS_SIZE];
+S_OVF = ( ~(ALU_IN1[BUS_SIZE-1] ^ ALU_IN2[BUS_SIZE-1])
+&& (ALU_OUT[BUS_SIZE-1] ^ ALU_IN1[BUS_SIZE-1]) );
+end
+SUB : begin
+ALU_OUT_W_CARRY = ALU_IN1 - ALU_IN2;
+ALU_OUT = ALU_OUT_W_CARRY[BUS_SIZE-1 : 0];
+CARRYOUT = ALU_OUT_W_CARRY[BUS_SIZE];
+ U_OVF = ~ALU_OUT_W_CARRY[BUS_SIZE];
+ S_OVF = ( (ALU_IN1[BUS_SIZE-1] ^ ALU_IN2[BUS_SIZE-1])
+ && ~(ALU_OUT[BUS_SIZE-1] ^ ALU_IN2[BUS_SIZE-1]) );
+ end
+ INC_IN1 : begin
+ ALU_OUT_W_CARRY = ALU_IN1 + 1'b1;
+ ALU_OUT = ALU_OUT_W_CARRY[BUS_SIZE-1 : 0];
+ CARRYOUT = ALU_OUT_W_CARRY[BUS_SIZE];
+ U_OVF = ALU_OUT_W_CARRY[BUS_SIZE];
+ end
+ 
+INC_IN2 : begin
+ALU_OUT_W_CARRY = ALU_IN2 + 1'b1;
+ALU_OUT = ALU_OUT_W_CARRY[BUS_SIZE-1 : 0];
+CARRYOUT = ALU_OUT_W_CARRY[BUS_SIZE];
+U_OVF = ALU_OUT_W_CARRY[BUS_SIZE];
+
+end
+PASS_0          : ALU_OUT = '0;
+PASS_IN2        : ALU_OUT = ALU_IN2;
+PASS_LOW16_IN2 : ALU_OUT = ALU_IN2[(BUS_SIZE/2)-1 :0];
+PASS_HIGH16_IN2 : ALU_OUT = ALU_IN2[BUS_SIZE-1 : (BUS_SIZE/2)];
+AND : ALU_OUT = ALU_IN1 & ALU_IN2;
+OR  : ALU_OUT = ALU_IN1 | ALU_IN2;
+XOR : ALU_OUT = ALU_IN1 ^ ALU_IN2;
+NAND  : ALU_OUT = ~(ALU_IN1 & ALU_IN2);
+NOR  : ALU_OUT = ~(ALU_IN1 | ALU_IN2);
+XNOR : ALU_OUT = ~(ALU_IN1 ^ ALU_IN2);
+NOT_IN2 : ALU_OUT = ~ ALU_IN2;
+L_SHIFT_IN2 : ALU_OUT = ALU_IN2 << 1;    
+  R_SHIFT_IN2 : ALU_OUT = ALU_IN2 >> 1;
+S_L_SHIFT_IN2 : ALU_OUT = ALU_IN2 <<< 1;
+S_R_SHIFT_IN2  : ALU_OUT = ALU_IN2 >>> 1;
+L_ROTATE_IN2 : ALU_OUT = {ALU_IN2[BUS_SIZE-2 : 0], ALU_IN2[BUS_SIZE-1]};
+R_ROTATE_IN2    : ALU_OUT = {ALU_IN2[0],ALU_IN2[BUS_SIZE-1 : 1]};
+
+default : ALU_OUT = 'x;
+endcase
+end : ALU
+
+assign MEM_DATA_IN = ALU_OUT;
+assign ALU_OUT_ZERO = ALU_OUT == '0 ? '1 : '0;
+assign ALU_OUT_POS = !ALU_OUT[BUS_SIZE-1] ? '1 : '0;
+assign ALU_OUT_NEG = ALU_OUT[BUS_SIZE-1] ? '1 : '0;
+   
+always_comb
+begin : MUX_DATA
+
+case (SEL_DATA)
+1'b0 : INT_DATA_BUS = MEM_DATA_OUT;
+1'b1 : INT_DATA_BUS = ALU_OUT;
+default : INT_DATA_BUS = 'x;
+endcase 
+end : MUX_DATA
+
+always_comb
+begin : MUX_SOURCE1
+case (SEL_SOURCE1)
+1'b0 : ALU_IN1 = {{27{PC_OUT[4]}},PC_OUT};
+1'b1 : ALU_IN1 = SOURCE_REG1;
+default : ALU_IN1 = 'x;
+endcase
+end : MUX_SOURCE1
+
+always_comb
+begin : MUX_SOURCE2
+
+case (SEL_SOURCE2)
+1'b0 : ALU_IN2 = SOURCE_REG2;
+1'b1 : ALU_IN2 = IMM_OFFSET_DATA;
+default : ALU_IN2 = 'x;
+endcase
+
+end : MUX_SOURCE2
+
+always_comb
+begin : MUX_ADDR
+
+case (SEL_ADDR)
+1'b0 : MEM_ADDRESS = PC_OUT;
+1'b1 : MEM_ADDRESS = ADDR_REG_OUT;
+default : MEM_ADDRESS = 'x;   
+endcase
+
+end : MUX_ADDR
+
 235 endmodule : DORITO_DATAPATH
